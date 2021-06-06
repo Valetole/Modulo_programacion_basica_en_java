7cb942ce4a7aa02922de97bb348902e5310c8737
Valetole <83321866+Valetole@users.noreply.github.com>
2021-06-06 17:00:43 -0400

    Add files via upload

---
+++ Día 29 (Jueves 03 Junio) Día 4 semana 6/ProductoController.java
@@ -0,0 +1,30 @@
+package controladores;
+
+import java.util.HashMap;
+
+import java.util.Map;
+
+import modelo.Producto;
+
+public class ProductoController {
+		static Map<String, Producto> productos = new HashMap<String,Producto>();
+		
+		
+		public String agregarProducto(String nombre, String tipoProducto, String descripcion, int precio) {
+						productos.put(nombre, new Producto(nombre, tipoProducto, descripcion, precio));
+						return "Nuevo producto agregado";
+	}
+		public Producto verProducto(String nombre) {
+			return productos.get(nombre);
+		}
+		public int modificarProducto(String nombre, String tipoProducto, String descripcion, int precio) {
+			Producto p = verProducto(nombre);
+			if(p!=null) {
+				p.setNombre(nombre);
+				p.setPrecio(precio);
+				return 1;
+			}else {
+			return 0;
+		}
+}
+}
---
+++ Día 29 (Jueves 03 Junio) Día 4 semana 6/ProductoControllerTest.java
@@ -0,0 +1,44 @@
+package controladores;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import modelo.Producto;
+
+class ProductoControllerTest {
+
+	@Test
+	void testAgregarProducto() {
+		ProductoController a = new ProductoController();
+		String respuesta = a.agregarProducto("Pan pita","Pan","Pan libre de manteca", 200);
+		String respuestaEsperada= "Nuevo producto agregado";//Hacer debug de mayusuculas para que pasen pruebas
+		assertEquals(respuestaEsperada,respuesta);
+	}
+	/*Cuando yo comparo un objeto con otro objeto a java le interesa
+	 * la referencia de memoria, es decir, donde se crean esos objetos, si creo uno en el controlador y 
+	 * otro en este set de pruebas aunque sean identicos el test me dará error.
+	 * para compararlos yo debiese hacerlo a través de sus atributos por ejemplo
+	 * con su getNombre(), o a través de todos sus atributos. 
+	 * Para tener pruebas requiero que retornen si o si. por ejemplo si tengo
+	 * un modificar producto puedo entregar un 1 producto modificado y 0 si no encontró ningun producto a modificar 
+	 */
+	@Test
+	void testVerProducto () {
+		ProductoController a = new ProductoController();
+		a.agregarProducto("Pan pita","Pan","Pan libre de manteca",200);//Se agrega el producto para poder buscarlo
+		Producto obtenido = a.verProducto("Pan pita");//creado en el constructor productoController
+		Producto esperado= new Producto("Pan pita","Pan","Pan libre de manteca",200);//creado aca en el test
+		assertEquals(esperado.getNombre(),obtenido.getNombre());//Puedo hacer un equals para cada atributo para compararlos todos.
+	}
+	
+	@Test
+	void testModificarProducto() {
+		ProductoController a = new ProductoController();
+		a.agregarProducto("Pan pita","Pan","Pan libre de manteca", 200);
+		int obtenido = a.modificarProducto("Pan pita","Pan","Pan libre de manteca",200);
+		int esperado =1;
+		assertEquals(esperado,obtenido);
+	}
+
+}
